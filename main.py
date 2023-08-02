66908b9a530d5bd9365a4aba27d072c00e3fae5d
Ahmad <mahmedraza90@gmail.com>
2023-08-03 02:50:43 +0500

    Completed Project

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def send_email(rate,message):
+def send_email(rate, message, previous):
     _ = load_dotenv()
     email_address_from = os.environ.get("send_email_from")
     email_password = os.environ.get("EMAIL_PASSWORD")
@@ -20,7 +20,7 @@ def send_email(rate,message):
     msg['From'] = email_address_from
     msg['To'] = email_address_to
     msg.set_content(
-        f"Dollar Rate dropped down to {rate} from previous {previous_dollar_rate}. The difference is of {previous_dollar_rate - rate}")
+        f"Dollar Rate dropped down to {rate} from previous {previous}. The difference is of {previous - rate}")
 
     # send email
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
@@ -28,32 +28,55 @@ def send_email(rate,message):
         smtp.send_message(msg)
 
 
-previous_dollar_rate = 285.5
-
-
 def dollar_rate():
     global previous_dollar_rate
     dollar_response = requests.get("https://www.google.com/search?q=1+dollar+to+pkr").text
     soup = BeautifulSoup(dollar_response, "html.parser")
-    rate = soup.find(name="div", class_="BNeawe iBp4i AP7Wnd").getText()
+    rate = soup.find(name="div", class_="BNeawe iBp4i AP7Wnd").getText().strip()
     try:
         float_rate = float(rate[0:6])
     except ValueError:
         float_rate = float(rate[0:5])
 
     if float_rate < previous_dollar_rate:
-        send_email(float_rate,"Dollar Rate Update")
-        previous_dollar_rate = float_rate
+        send_email(float_rate, "Dollar Rate Update", previous_dollar_rate)
+        dollar_file = open("dollar_rate.txt", "w")
+        dollar_file.write(str(float_rate))
 
-previous_petrol_price = 272.95
+
+previous_petrol_price = previous_dollar_rate = 0
+
+
 def petrol_price():
     global previous_petrol_price
-    petrol_resource = requests.get("https://www.psopk.com/").text
+    petrol_response = requests.get("https://www.psopk.com/").text
+    soup2 = BeautifulSoup(petrol_response, "html.parser")
+    rate = soup2.find(name="div", class_="uk-width-auto").getText().strip()
+    try:
+        float_rate = float(rate[3:9])
+    except ValueError:
+        float_rate = float(rate[3:8])
+
+    if float_rate < previous_petrol_price:
+        send_email(float_rate, "Petrol Price Update", previous_petrol_price)
+        petrol_file = open("petrol_rate.txt", "w")
+        petrol_file.write(str(float_rate))
+
+
+def set_globals():
+    global previous_dollar_rate, previous_petrol_price
+    dollar_file = open("dollar_rate.txt", "r")
+    previous_dollar_rate = float(dollar_file.readline())
+    petrol_file = open("petrol_rate.txt", "r")
+    previous_petrol_price = float(petrol_file.readline())
+
 
 
 def main():
+    set_globals()
+    petrol_price()
     dollar_rate()
-
+    print("Program successfully executed")
 
 
 if __name__ == "__main__":
